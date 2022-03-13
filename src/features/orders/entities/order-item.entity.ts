7592d430a2f3db7af31ca6b5e19158986ddfbcc5
lokicoule <lokicoule@yahoo.fr>
2022-03-13 15:44:48 +0100

    refactor(order-item.entity) : extends GqlEntity

--- src/features/orders/entities/order-item.entity.ts
+++ src/features/orders/entities/order-item.entity.ts
@@ -1,11 +1,12 @@
 import { Field, ObjectType } from '@nestjs/graphql';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Schema as MongooseSchema } from 'mongoose';
+import { GqlEntity } from 'src/core/models/entity/entity.graphql';
 import { Product } from 'src/features/products/entities/product.entity';
 
-@Schema({ _id: false })
+@Schema()
 @ObjectType()
-export class OrderItem {
+export class OrderItem extends GqlEntity {
   @Field(() => Product, { nullable: true })
   @Prop({
     type: MongooseSchema.Types.ObjectId,
